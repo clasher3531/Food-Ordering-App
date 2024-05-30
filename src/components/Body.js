f6e14554245dab007b0743d02ee0225313b9b78d
C_L_A_S_H_E_R <nikhil.a.joshi@capgemini.com>
2024-05-30 18:30:31 +0530

    Adding CSS

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,16 @@ import resList from "../utils/mockData";
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">search</div>
+      <div className="search">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search Resturants"
+        ></input>
+        <div className="search-button">
+          <button type="submit">Search</button>
+        </div>
+      </div>
       <div className="res-list">
         {resList.map((resturant) => (
           <ResturantCard key={resturant.info.id} resData={resturant} />
