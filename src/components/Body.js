37b88866b251ec894496ff911dea3cd8d8ce598b
clasher3531 <nikhil.a.joshi@capgemini.com>
2024-06-01 14:53:12 -0700

    Search button

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,24 @@ import { useState } from "react";
 
 const Body = () => {
   const [listOfResturant, setListOfResturant] = useState(resList);
+  const [inputVal, setInputVal] = useState("");
   function topRatedBtnClickHandler() {
     let filteredList = listOfResturant.filter(
       (resturant) => resturant.info.avgRating > 4.2
     );
+    setListOfResturant(filteredList);
+  }
+  function changeInputHandler(event) {
+    setInputVal(event.target.value);
+  }
+  function searchButtonClickHandler() {
+    var inputValLength = inputVal.length;
+    let filteredList = resList.filter((resturant) => {
+      return (
+        resturant.info.name.substring(0, inputValLength).toLowerCase() ===
+        inputVal.toLowerCase()
+      );
+    });
     setListOfResturant(filteredList);
   }
   return (
@@ -17,9 +31,13 @@ const Body = () => {
           type="text"
           className="search-input"
           placeholder="Search Resturants"
+          value={inputVal}
+          onChange={changeInputHandler}
         ></input>
         <div className="search-button">
-          <button type="submit">Search</button>
+          <button type="submit" onClick={searchButtonClickHandler}>
+            Search
+          </button>
         </div>
       </div>
       <button className="top-rated-btn" onClick={topRatedBtnClickHandler}>
