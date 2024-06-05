ada80fa8777b2a0bca0ccf6c0022923c33ce50cd
clasher3531 <nikhil.a.joshi@capgemini.com>
2024-06-05 19:43:42 +0530

    React Router Implementation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
   return (
@@ -12,5 +16,21 @@ const AppLayout = () => {
   );
 };
 
+const reactRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/About",
+    element: <About />,
+  },
+  {
+    path: "/Contact",
+    element: <Contact />,
+  },
+]);
+
 var root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />); // job is to convert heading object into h1 tag and append into root
+root.render(<RouterProvider router={reactRouter} />);
---
+++ src/components/Error.js
@@ -0,0 +1,16 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const error = useRouteError();
+  return (
+    <div className="error-container">
+      <h1>Oops!!!!!!!</h1>
+      <h2>Something went wrong</h2>
+      <h3>
+        {error.status} : {error.statusText}
+      </h3>
+    </div>
+  );
+};
+
+export default Error;
